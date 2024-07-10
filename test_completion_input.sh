7372cbd8c7283cc001c202c9c4126cb18a5f653d
Pietro <peter.caforio@gmail.com>
2024-07-10 19:17:25 +0200

    less parameters

--- test_completion_input.sh
+++ test_completion_input.sh
@@ -1,1 +1,1 @@
-python test_completion.py --category car --model output/train_completion/2024-07-06-23-48-35/epoch_199.pth --dataroot_sv /cluster/54/tob_pit_copy --batch_size 8
+python test_completion.py --category car --model output/train_completion/2024-07-09-08-38-19/epoch_499.pth --dataroot_sv /cluster/54/tob_pit_copy --batch_size 8 
